95aa0281dc4ffa2c393a4d80c72f50c7ec6b0a15
Moaz Muhammad <moazmo27@gmail.com>
2025-07-16 12:54:25 +0300

    Clean up - remove unnecessary build.sh and render.yaml files

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Building Hotel Booking Predictor..."
-echo "Installing Python dependencies..."
-pip install -r requirements.txt
-echo "Creating ML models..."
-python extract_model.py
-echo "Build complete!"
